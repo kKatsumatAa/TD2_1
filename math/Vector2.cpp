9280a0a911ec2ddcdf70d3a2fc825fa9c9a9a007
LE2A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-10-07 10:37:29 +0900

    vector2追加

---
+++ math/Vector2.cpp
@@ -0,0 +1,113 @@
+#include "Vector2.h"
+#include <DirectXMath.h>
+
+
+float Vector2::GetLength() const
+{
+	return sqrtf(x * x + y * y);
+}
+
+void Vector2::Normalized()
+{
+	*this /= GetLength();
+}
+
+Vector2 Vector2::GetNormalize()
+{
+	float len = GetLength();
+	if (len > 0) return *this / len;
+
+	return *this;
+}
+
+float Vector2::Cross(const Vector2& other)const
+{
+	return x * other.y - y * other.x;
+}
+
+float Vector2::Dot(const Vector2& other)const
+{
+	return x * other.x + y * other.y;
+}
+
+
+
+//-----------------------------------------------------------------
+//演算子オーバーロード
+Vector2 Vector2::operator-()
+{
+	return { -x,-y };
+}
+
+Vector2 Vector2::operator+()
+{
+	return *this;
+}
+
+
+Vector2 Vector2::operator*(const float& other)
+{
+	Vector2 v;
+	v.x = x * other;
+	v.y = y * other;
+
+	return v;
+}
+
+Vector2& Vector2::operator*=(const float& other)
+{
+	*this = *this * other;//thisはアドレスなので*つける
+
+	return *this;
+}
+
+
+
+Vector2 Vector2::operator/(const float& other)
+{
+	Vector2 v;
+	v.x = x / other;
+	v.y = y / other;
+
+	return v;
+}
+
+Vector2& Vector2::operator/=(const float& other)
+{
+	*this = *this / other;//thisはアドレスなので*つける
+
+	return *this;
+}
+
+
+
+
+const Vector2 operator+(const Vector2& v1, const Vector2& v2)
+{
+	Vector2 temp(v1);
+	return temp += v2;
+}
+
+const Vector2 operator-(const Vector2& v1, const Vector2& v2)
+{
+	Vector2 temp(v1);
+	return temp -= v2;
+}
+
+const Vector2 operator*(const Vector2& v, float s)
+{
+	Vector2 temp(v);
+	return temp *= s;
+}
+
+const Vector2 operator*(float s, const Vector2& v)
+{
+	Vector2 temp(v);
+	return temp *= s;
+}
+
+const Vector2 operator/(const Vector2& v, float s)
+{
+	Vector2 temp(v);
+	return temp /= s;
+}
--- math/Vector2.h
+++ math/Vector2.h
@@ -13,13 +13,29 @@ class Vector2 {
 	Vector2();                          // 零ベクトルとする
 	Vector2(float x, float y); // x成分, y成分 を指定しての生成
 
-	// 単項演算子オーバーロード
-	Vector2 operator+() const;
-	Vector2 operator-() const;
+	//メンバ関数
+	float GetLength() const;
+	void  Normalized();
+	Vector2  GetNormalize();
+	float Dot(const Vector2& v) const;
+	float Cross(const Vector2& v) const;
 
-	// 代入演算子オーバーロード
-	Vector2& operator+=(const Vector2& v);
-	Vector2& operator-=(const Vector2& v);
-	Vector2& operator*=(float s);
-	Vector2& operator/=(float s);
+	Vector2  operator -();//vec同士の引き算
+	Vector2& operator -=(const Vector2& other);//複合演算子(-)
+
+	Vector2  operator +();
+	Vector2& operator +=(const Vector2& other);//複合演算子(+)
+
+	Vector2  operator *(const float& other);
+	Vector2& operator *=(const float& other);//複合演算子(*)
+
+	Vector2  operator /(const float& other);
+	Vector2& operator /=(const float& other);//複合演算子(/)
 };
+
+//二項演算子オーバーロード
+const Vector2 operator +(const Vector2& v1, const Vector2& v2);
+const Vector2 operator -(const Vector2& v1, const Vector2& v2);
+const Vector2 operator *(const Vector2& v, float s);
+const Vector2 operator *(float s, const Vector2& v);
+const Vector2 operator /(const Vector2& v, float s);
