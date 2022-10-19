7a3e145d7047002fa72cd54942270164fe46b46c
JunyaSugita <94422859+JunyaSugita@users.noreply.github.com>
2022-10-19 18:10:23 +0900

    スローモーションに変更

--- Gravity.cpp
+++ Gravity.cpp
@@ -106,30 +106,31 @@ Vector3 Gravity::Move(Vector3 pos, float gavitySpeed, float playerSprrd)
 
 Vector3 Gravity::ObjMove(WorldTransform world, float gavitySpeed)
 {
-	if (!sugitaIsGomi) {
-		if (axis == Axis::DOWN) {
-			world.translation_.y -= gavitySpeed;
-			if (world.translation_.y < -18 + world.scale_.y + 2) {
-				world.translation_.y += gavitySpeed;
-			}
-		}
-		else if (axis == Axis::UP) {
+	if (sugitaIsGomi) {
+		gavitySpeed *= 0.2f;
+	}
+	if (axis == Axis::DOWN) {
+		world.translation_.y -= gavitySpeed;
+		if (world.translation_.y < -18 + world.scale_.y + 2) {
 			world.translation_.y += gavitySpeed;
-			if (world.translation_.y > 18 - world.scale_.y - 2) {
-				world.translation_.y -= gavitySpeed;
-			}
 		}
-		else if (axis == Axis::RIGHT) {
-			world.translation_.x += gavitySpeed;
-			if (world.translation_.x > 3 - world.scale_.x - 2) {
-				world.translation_.x -= gavitySpeed;
-			}
+	}
+	else if (axis == Axis::UP) {
+		world.translation_.y += gavitySpeed;
+		if (world.translation_.y > 18 - world.scale_.y - 2) {
+			world.translation_.y -= gavitySpeed;
 		}
-		else {
+	}
+	else if (axis == Axis::RIGHT) {
+		world.translation_.x += gavitySpeed;
+		if (world.translation_.x > 3 - world.scale_.x - 2) {
 			world.translation_.x -= gavitySpeed;
-			if (world.translation_.x < -33 + world.scale_.x + 2) {
-				world.translation_.x += gavitySpeed;
-			}
+		}
+	}
+	else {
+		world.translation_.x -= gavitySpeed;
+		if (world.translation_.x < -33 + world.scale_.x + 2) {
+			world.translation_.x += gavitySpeed;
 		}
 	}
 	return world.translation_;
