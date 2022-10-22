83b4aa84a7ffda0f5eec86df5512946e870c9ccb
チャン スギ <junjunchan64@gmail.com>
2022-10-22 13:47:58 +0900

    突進先を表示

--- Guide.cpp
+++ Guide.cpp
@@ -3,14 +3,72 @@
 void Guide::Initialize(Model* model)
 {
 	model_ = model;
+	for (int i = 0; i < Num; i++) {
+		pos_[i].Initialize();
+	}
+	wallStart_[0] = stageLeftTop;
+	wallEnd_[0] = stageRightTop;
+	wallStart_[1] = stageLeftTop;
+	wallEnd_[1] = stageLeftBottom;
+	wallStart_[2] = stageRightTop;
+	wallEnd_[2] = stageRightBottom;
+	wallStart_[3] = stageLeftBottom;
+	wallEnd_[3] = stageRightBottom;
 }
 
-void Guide::Update()
+void Guide::Update(Vector3 start, Vector3 end)
 {
-
+	for (int i = 1; i < Num; i++) {
+		start += end;
+		pos_[i].translation_ = start;
+		pos_[i].scale_ = { 1.0f,1.0f,1.0f };
+		pos_[i].UpdateMatrix();
+		if (pos_[i].translation_.x <= wallStart_[0].x) {
+			pos_[i].scale_ = { 10.0f,10.0f,1.0f };
+			pos_[i].translation_.x = wallStart_[0].x;
+			pos_[i].UpdateMatrix();
+			for (int j = i + 1; j < Num; j++) {
+				pos_[j].translation_ = { -100,-100,-100 };
+				pos_[j].UpdateMatrix();
+			}
+			break;
+		}
+		else if (pos_[i].translation_.x >= wallEnd_[0].x) {
+			pos_[i].scale_ = { 10.0f,10.0f,1.0f };
+			pos_[i].translation_.x = wallEnd_[0].x;
+			pos_[i].UpdateMatrix();
+			for (int j = i + 1; j < Num; j++) {
+				pos_[j].translation_ = { -100,-100,-100 };
+				pos_[j].UpdateMatrix();
+			}
+			break;
+		}
+		else if (pos_[i].translation_.y <= wallStart_[0].y) {
+			pos_[i].scale_ = { 10.0f,10.0f,1.0f };
+			pos_[i].translation_.y = wallStart_[0].y;
+			pos_[i].UpdateMatrix();
+			for (int j = i + 1; j < Num; j++) {
+				pos_[j].translation_ = { -100,-100,-100 };
+				pos_[j].UpdateMatrix();
+			}
+			break;
+		}
+		else if (pos_[i].translation_.y >= wallStart_[3].y) {
+			pos_[i].scale_ = { 10.0f,10.0f,1.0f };
+			pos_[i].translation_.y = wallStart_[3].y;
+			pos_[i].UpdateMatrix();
+			for (int j = i + 1; j < Num; j++) {
+				pos_[j].translation_ = { -100,-100,-100 };
+				pos_[j].UpdateMatrix();
+			}
+			break;
+		}
+	}
 }
 
 void Guide::Draw(ViewProjection view)
 {
-	model_->Draw(pos_,view);
+	for (int i = 0; i < Num; i++) {
+		model_->Draw(pos_[i], view);
+	}
 }
--- Guide.h
+++ Guide.h
@@ -6,23 +6,19 @@ class Guide
 {
 public:
 	void Initialize(Model* model);
-	void Update();
+	void Update(Vector3 start, Vector3 end);
 	void Draw(ViewProjection view);
 
 private:
-	WorldTransform pos_;
+	static const int Num = 60;
+	WorldTransform pos_[Num];
 	Vector3 playerStart_;
 	Vector3 playerEnd_;
-	Vector3 stageLeftTop = Vector3(-33.0f, -18.0f, 0.0f);
+	Vector3 stageLeftTop = Vector3(-32.0f, -18.0f, 0.0f);
 	Vector3 stageRightTop = Vector3(3.0f, -18.0f,0.0f);
-	Vector3 stageLeftBottom = Vector3(-33.0f, 18.0f,0.0f);
+	Vector3 stageLeftBottom = Vector3(-32.0f, 18.0f,0.0f);
 	Vector3 stageRightBottom = Vector3(3.0f, 18.0f,0.0f);
-	Vector3 WallStart_[4];
-	Vector3 WallEnd_[4];
+	Vector3 wallStart_[4];
+	Vector3 wallEnd_[4];
 	Model* model_;
 };
-
-static Vector2 stageLeftTop = Vector2(-33.0f, -18.0f);
-static Vector2 stageRightTop = Vector2(3.0f, -18.0f);
-static Vector2 stageLeftBottom = Vector2(-33.0f, 18.0f);
-static Vector2 stageRightBottom = Vector2(3.0f, 18.0f);
